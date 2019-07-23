2e6d71fea70b31153b914e758423e8330f730ae9
juger rash <jugerrash@jugers-Mac.local>
2019-07-23 05:59:18 -0700

    gitignore File added

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by juger rash on 23.07.19.
+//  Copyright © 2019 juger rash. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
